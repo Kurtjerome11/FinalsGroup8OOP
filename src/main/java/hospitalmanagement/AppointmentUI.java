f58751384f9d336cea3cf72fa9bc1536029dc3ff
HerreraAngelica <gelherrera02@gmail.com>
2024-07-01 14:29:15 +0800

    Herrera, Angelica L.

--- src/main/java/hospitalmanagement/AppointmentUI.java
+++ src/main/java/hospitalmanagement/AppointmentUI.java
@@ -18,7 +18,7 @@ public class AppointmentUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
       
-        JLabel background = new JLabel(new ImageIcon("C:\\Users\\angge\\Downloads\\hospi.png"));
+        JLabel background = new JLabel(new ImageIcon("C:Images/hospi.png"));
         background.setBounds(0, 0, 1300, 800); 
         background.setLayout(null);
 
